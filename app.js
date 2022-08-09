fb58bfebcb5fec1bcfec50f5d01639e9657e57f2
rattenreiter <martin.meier@hu-berlin.de>
2022-08-09 17:37:09 +0200

    Integration of Passport and Session First Step

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // REQUIRES
 // ---------------------------------------------------
 const express = require('express');
+const session = require('express-session');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
 
 // SETUP
 // ---------------------------------------------------
@@ -12,7 +16,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-const salt_rounds = 10;
+
 
 // DB SETUP
 // ---------------------------------------------------
@@ -35,50 +39,9 @@ app.route('/')
     });
 
 app.route('/register')
-    .get((req, res) => {
-        res.render('register');
-    })
-    .post((req, res) => {
-        bcrypt.hash(req.body.password, salt_rounds, (err, hash) => {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save((err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect('/secrets');
-                }
-            });
-        });
-    });
 
 app.route('/login')
-    .get((req, res) => {
-        res.render('login',{wrongNotice: '', userName: ''});
-    })
-    .post((req, res) => {
-        User.findOne({email:req.body.username}, (err, foundUser) => {
-            if (foundUser) {
-               bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
-                    if (result === true) {
-                        res.redirect('/secrets');
-                    } else {
-                        res.render('login', {
-                            wrongNotice: 'Password incorrect', 
-                            userName: req.body.username
-                        });
-                    }
-               })
-            } else {
-                res.render('login', {
-                    wrongNotice: 'Username (EMail) not found',
-                    userName: ''
-                });
-            }
-        });
-    });
+
 
 app.route('/secrets')
     .get((req, res) => {
